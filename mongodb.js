a0138709ca7cbfb0eb4b8a309d0d47714e3c7b71
Kshitiz Goel <57189655+kshitiz-goel07@users.noreply.github.com>
2020-09-03 23:14:27 +0530

    Create and read

--- mongodb.js
+++ mongodb.js
@@ -1,9 +1,15 @@
 // CRUD create read update delete
 
-const { MongoClient , ObjectId } = require('mongodb')
+const { MongoClient , ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+// const id = new ObjectID()
+// console.log(id)
+// console.log(id.getTimestamp())
+// console.log(id.id.length)
+// console.log(id.toHexString().length)
 
 MongoClient.connect(connectionURL , {useNewUrlParser : true} , (error,client) => {
 if(error){
@@ -11,47 +17,19 @@ if(error){
 }
 const db = client.db(databaseName)
 
-// db.collection('users').insertOne({
-//     name : 'Kshitiz Goel',
-//     age : 20
-// }, (error, result) => {
-//       if(error){
-//           return console.log("Can't update to table")
-//       }
-//       console.log(result.ops)
-// })
-// db.collection('users').insertMany([
-//     {
-//         name : 'vansh',
-//         age : 21
-//     },
-//     {
-//         name : 'Ritik',
-//         age : 22
-//     }
-// ],(error, result)=>{
-//     if(error){
-//         return console.log('Unable to update table')
-//     }
-//     console.log(result.ops)
-// })
-db.collection('tasks').insertMany([
-    {
-        description : "Cleaned house",
-        complete : true
-    },
-    {
-        description : "Cleaned car",
-        complete : true
-    },
-    {
-        description : "painting done",
-        complete : false
-    }
-],(error,result)=>{
-    if(error){
-        return console.log("Unable to update")
-    }
-    console.log(result.ops)
+// db.collection('users').findOne({name:'Ritik'}, (error, user)=>{
+//            if(error){
+//                return console.log('unable to fetch !')
+//            }
+//            console.log(user)
+
+db.collection('tasks').findOne({_id : new ObjectID("5f4e29610d18bb22ac2c795f") },(error , task)=>{
+console.log(task) 
+})
+
+
+
+db.collection('tasks').find({complete : true}).toArray((error , task)=>{
+console.log(task)
 })
 })
